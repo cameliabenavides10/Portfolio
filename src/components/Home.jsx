7b8989d0dfc82550d9fb9e196b28482ff1d9cd8f
cameliabenavides10 <cameliabenavides@gmail.com>
2023-04-07 20:33:02 -0500

    more styling of the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,17 +11,16 @@ const Home = () => {
 
         <div name='home' className='w-full h-screen '>
 
-<Canvas style={{ position: 'absolute', zIndex: -1, background: '#0a192f' }} />
+            <Canvas style={{ position: 'absolute', zIndex: -1, background: '#0a192f' }} />
             {/* container */}
 
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-
                 <p className='text-pink-600'>Hi, my name is</p>
-                <h1> Camelia Benavides</h1>
-                <h2> I am a Full Stack Developer.</h2>
-                <p>I do stuff</p>
+                <h1 className='text-4xl  sm:text-7xl font-bold text-[#ccd6f6]'> Camelia Benavides</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] '> I am a Full Stack Developer.</h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>I do stuff</p>
                 <div>
-                    <button>View Work <HiArrowNarrowRight /> </button>
+                    <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work <HiArrowNarrowRight /> </button>
 
                 </div>
             </div>
